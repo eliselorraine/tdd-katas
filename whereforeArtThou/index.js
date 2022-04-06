8816631293285db11493f0b9617a6e251aa730f8
Elise Lawrence <eliselorraine@icloud.com>
2022-04-06 09:48:20 +0200

    another test passed

--- whereforeArtThou/index.js
+++ whereforeArtThou/index.js
@@ -1,30 +1,46 @@
 const whatIsInAName = (collection, source) => {
   const sourceKeys = Object.keys(source);
-  console.log(source.length);
   let arr = [];
   if (sourceKeys.length === 1) {
-      arr = collection.filter((obj) => obj.last === source.last);
-      return arr;
+    arr = collection.filter((obj) => obj.last === source.last);
+    return arr;
   } else if (sourceKeys.length > 1) {
-      arr = collection.filter((obj) => obj.last == source.last);
+    arr = matches(collection, source);
+    return arr;
   }
   return arr;
 };
 
 const matches = (collection, source) => {
-    for(let i = 0; i < collection.length; i++) {
-        
+  let answer = [];
+  const sourceKeys = Object.keys(source);
+  const sourceValues = Object.values(source);
+  collection.forEach((obj, i) => {
+    const collectionKeys = Object.keys(obj);
+    const collectionValues = Object.values(obj);
+    for (let j = 0; j < collectionKeys.length; j++) {
+      if (
+        collectionKeys[j] === sourceKeys[j] &&
+        collectionValues[j] === sourceValues[j]
+      ) {
+        answer.push(collection[i]);
+        return;
+      } 
     }
-}
-let pairs = whatIsInAName(
-[
-    { "apple": 1, "bat": 2 }, 
-    { "bat": 2 }, 
-    { "apple": 1, "bat": 2, "cookie": 2 }
-], 
-    { "apple": 1, "bat": 2 }
-) 
+  });
+  return answer;
+};
+let morePairs = whatIsInAName(
+    [
+      { "apple": 1, "bat": 2 }, 
+      { "apple": 1 }, 
+      { "apple": 1, "bat": 2, "cookie": 2 }
+    ], 
+    { "apple": 1, "cookie": 2 }
+    ) 
 
-console.log(pairs);
+console.log(
+  morePairs
+);
 
 module.exports = whatIsInAName;
--- whereforeArtThou/index.test.js
+++ whereforeArtThou/index.test.js
@@ -1,55 +1,57 @@
 const whatIsInAName = require("./index");
 
-let array = whatIsInAName([{ first: "Romeo", last: "Montague" }], {
-  first: "apple",
-});
-
-let shakespeare = whatIsInAName(
-  [
-    { first: "Romeo", last: "Montague" },
-    { first: "Mercutio", last: null },
-    { first: "Tybalt", last: "Capulet" },
-  ],
-  { last: "Capulet" }
-);
-
-let fruits = whatIsInAName(
-[
-  { apple: 1 }, 
-  { apple: 1 }, 
-  { apple: 1, bat: 2 }
-], 
-{ apple: 1 }
-);
-
-let pairs = whatIsInAName(
-  [
-    { apple: 1, bat: 2 }, 
-    { bat: 2 }, 
-    { apple: 1, bat: 2, cookie: 2 }
-  ],
-  { apple: 1, bat: 2 }
-);
-
-console.log(pairs);
-
 describe("Wherefore art thou", () => {
+  let array = whatIsInAName([{ first: "Romeo", last: "Montague" }], {
+    first: "apple",
+  });
   it("returns an array", () => {
     expect(array).toStrictEqual([]);
   });
 
   it("should return the correct user", () => {
+    let shakespeare = whatIsInAName(
+      [
+        { first: "Romeo", last: "Montague" },
+        { first: "Mercutio", last: null },
+        { first: "Tybalt", last: "Capulet" },
+      ],
+      { last: "Capulet" }
+    );
     const expected = [{ first: "Tybalt", last: "Capulet" }];
     expect(shakespeare).toStrictEqual(expected);
   });
 
-  it("should return the correct fruits", () => {
+  it("should handle objects in the collection with multiple keys", () => {
+    let fruits = whatIsInAName(
+      [{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }],
+      { apple: 1 }
+    );
     const expected = [{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }];
     expect(fruits).toStrictEqual(expected);
   });
 
-  it("should handle objects with multiple keys", () => {
-    const expected = [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]
-    expect(fruits).toStrictEqual(expected);
+  it("should handle objects in the source with multiple keys", () => {
+    let pairs = whatIsInAName(
+      [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+      { apple: 1, bat: 2 }
+    );
+    const expected = [
+      { apple: 1, bat: 2 },
+      { apple: 1, bat: 2, cookie: 2 },
+    ];
+    expect(pairs).toStrictEqual(expected);
   });
+
+  it("should not be affected by order of key-value pairs", () => {
+    let morePairs = whatIsInAName(
+      [
+        { "apple": 1, "bat": 2 }, 
+        { "apple": 1 }, 
+        { "apple": 1, "bat": 2, "cookie": 2 }
+      ], 
+      { "apple": 1, "cookie": 2 }
+      ) 
+      const expected = [{ "apple": 1, "bat": 2, "cookie": 2 }]
+      expect(morePairs).toStrictEqual(expected);
+  })
 });
