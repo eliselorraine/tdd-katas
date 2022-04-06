5bb358cfd450feada2db06876e1d338075de0fa2
Elise Lawrence <eliselorraine@icloud.com>
2022-04-06 11:51:14 +0200

    spinal case solution

--- spinalCase/index.js
+++ spinalCase/index.js
@@ -1,5 +1,12 @@
 const spinalCase = (str) => {
-    return str;
-}
+  const regex = /[^a-zA-Z0-9]/gm;
+  const regexTest = /(?=[A-Z])(?!\b)/gm; // positive lookahead, negative lookbehind for lowercaseCapital boundaries
+  let result = str.replaceAll(regex, "-");
+  if (regexTest.test(str)) {
+    result = result.replace(regexTest, "-");
+  }
+  result = result.toLowerCase();
+  return result;
+};
 
 module.exports = spinalCase;
--- spinalCase/index.test.js
+++ spinalCase/index.test.js
@@ -5,4 +5,16 @@ describe('spinal case', () => {
         const expected = "this-is-spinal-tap";
         expect(spinalCase("This Is Spinal Tap")).toStrictEqual(expected);
     })
+    it('should handle more than just spaces', () => {
+        const expected = "the-andy-griffith-show";
+        expect(spinalCase("The_Andy_Griffith_Show")).toStrictEqual(expected); 
+    })
+    it('should handle already existing hyphens', () => {
+        const expected = "teletubbies-say-eh-oh";
+        expect(spinalCase("Teletubbies say Eh-oh")).toStrictEqual(expected); 
+    })
+    it('should handle word breaks without spaces', () => {
+        const expected = "all-the-small-things";
+        expect(spinalCase("AllThe-small Things")).toStrictEqual(expected); 
+    })
 })
