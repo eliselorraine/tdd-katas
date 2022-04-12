e20ca1e161daa935a29077604b4dd4568be09a88
Elise Lawrence <eliselorraine@icloud.com>
2022-04-12 17:10:15 +0200

    roman numeral solution

--- romanNumeralConverter/index.js
+++ romanNumeralConverter/index.js
@@ -1,34 +1,63 @@
 const romanNumeral = (num) => {
-  let ans;
-  let arr = [];
   let str = num.toString();
-  const lastDigit = str[str.length - 1];
-    console.log(lastDigit);
+  if (str.length === 1) {
+    return singleNumeral(num);
+  }
+  let tens = str.split("");
+  let x = "";
+  for (let i = tens.length - 1; i >= 0; i--) {
+    tens[i] = tens[i].concat(x);
+    x = x.concat("0");
+  }
+
+  let numeralArr = tens.map((ten) => singleNumeral(ten));
+  let numeral = numeralArr.join("");
+  return numeral;
+};
+
+const singleNumeral = (num) => {
+  let numeral;
   for (key in romanLibrary) {
     if (key == num) {
-        ans = romanLibrary[key]; 
-        return ans; 
-    } 
+      numeral = romanLibrary[key];
+      return numeral;
+    }
   }
-  return ans;
+  return numeral;
 };
 
 const romanLibrary = {
-    '1': 'I',
-    '2': 'II',
-    '3': 'III',
-    '4': 'IV',
-    '5': 'V',
-    '9': 'IX',
-    '10': 'X',
-
-
-    '50': 'L',
-    '100': 'C',
-    '500': 'D',
-    '1000': 'M',
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  20: "XX",
+  30: "XXX",
+  40: "XL",
+  50: "L",
+  60: "LX",
+  70: "LXX",
+  80: "LXXX",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  600: "DC",
+  700: "DCC",
+  800: "DCCC",
+  900: "CM",
+  1000: "M",
+  2000: "MM",
+  3000: "MMM",
 };
 
-console.log(romanNumeral(12));
+console.log(romanNumeral(509));
+// console.log(singleNumeral(2));
 
 module.exports = romanNumeral;
--- romanNumeralConverter/index.test.js
+++ romanNumeralConverter/index.test.js
@@ -19,4 +19,64 @@ describe("the roman numeral converter function", () => {
     it("should handle numbers greater than 10", () => {
       expect(romanNumeral(12)).toStrictEqual("XII");
     });
+    it("should handle numbers greater than 10", () => {
+      expect(romanNumeral(16)).toStrictEqual("XVI");
+    });
+    it("should handle numbers greater than 20", () => {
+      expect(romanNumeral(29)).toStrictEqual("XXIX");
+    });
+    it("should handle numbers greater than 20", () => {
+      expect(romanNumeral(44)).toStrictEqual("XLIV");
+    });
+    it("should handle numbers greater than 20", () => {
+      expect(romanNumeral(45)).toStrictEqual("XLV");
+    });
+    it("should handle numbers greater than 50", () => {
+      expect(romanNumeral(68)).toStrictEqual("LXVIII");
+    });
+    it("should handle numbers greater than 50", () => {
+      expect(romanNumeral(83)).toStrictEqual("LXXXIII");
+    });
+    it("should handle numbers greater than 50", () => {
+      expect(romanNumeral(97)).toStrictEqual("XCVII");
+    });
+    it("should handle numbers greater than 50", () => {
+      expect(romanNumeral(99)).toStrictEqual("XCIX");
+    });
+    it("should handle numbers greater than 100", () => {
+      expect(romanNumeral(400)).toStrictEqual("CD");
+    });
+    it("should handle numbers greater than 500", () => {
+      expect(romanNumeral(500)).toStrictEqual("D");
+    });
+    it("should handle numbers greater than 500", () => {
+      expect(romanNumeral(501)).toStrictEqual("DI");
+    });
+    it("should handle numbers greater than 500", () => {
+      expect(romanNumeral(649)).toStrictEqual("DCXLIX");
+    });
+    it("should handle numbers greater than 500", () => {
+      expect(romanNumeral(798)).toStrictEqual("DCCXCVIII");
+    });
+    it("should handle numbers greater than 500", () => {
+        expect(romanNumeral(891)).toStrictEqual("DCCCXCI");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(1000)).toStrictEqual("M");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(1004)).toStrictEqual("MIV");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(1006)).toStrictEqual("MVI");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(1023)).toStrictEqual("MXXIII");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(2014)).toStrictEqual("MMXIV");
+    });
+    it("should handle numbers greater than 1000", () => {
+      expect(romanNumeral(3999)).toStrictEqual("MMMCMXCIX");
+    });
 });
