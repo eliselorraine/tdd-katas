9ae29aec7e46b3200036c170c08f503af89aafd1
Elise Lawrence <eliselorraine@icloud.com>
2022-04-19 09:47:17 +0200

    telephone solution

--- telephone/index.js
+++ telephone/index.js
@@ -1,20 +1,14 @@
 const telephoneCheck = (str) => {
-  let split = str.split("");
-  const re = /\d/;
-  const numbersOnly = split.filter((el) => re.test(el) === true);
-  const format = /1?\d{10}/gm;
-  console.log({
-    numbersOnly,
-  });
-  const joined = numbersOnly.join("");
-  if (format.test(joined)) {
+  const split = str.split("");
+  const whitespace = /\s/
+  const noWhiteSpace = split.filter(el => whitespace.test(el) === false).join("");
+  const telephone = /^1?(([(]\d{3}[)])|(\d{3}[-]?))\d{3}\W?\d{4}$/gm
+  if (telephone.test(noWhiteSpace)) {
     return true;
   }
   return false;
 };
 
-console.log(telephoneCheck("1 (123) 456-7890"));
 
 module.exports = telephoneCheck;
 
-// /^1?((\W{2}\d{3}\W{2})|(\W{1}\d{3}\W{1}))\d{3}\W\d{4}/gm
--- telephone/index.test.js
+++ telephone/index.test.js
@@ -46,4 +46,43 @@ describe("the telephone check function", () => {
   it("should handle multiple formats", () => {
     expect(telephoneCheck("2 (757) 622-7382")).toBe(false);
   });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("0 (757) 622-7382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("-1 (757) 622-7382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("2 757 622-7382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("10 (757) 622-7382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("27576227382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("(275)76227382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("2(757)6227382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("2(757)622-7382")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("555)-555-5555")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("(555-555-5555")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("(555)5(55?)-5555")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("55 55-55-555-5")).toBe(false);
+  });
+  it("should handle multiple formats", () => {
+    expect(telephoneCheck("11 555-555-5555")).toBe(false);
+  });
 });
